f101726d6db10834edc4595e1ea1f6119d5f5f7f
ferdyciputra <ferdyarmstrong@gmail.com>
2024-02-12 19:47:03 +0700

    added press ENTER for method searchKKeyword

--- src/test/java/pageobjects/HomePage.java
+++ src/test/java/pageobjects/HomePage.java
@@ -9,7 +9,6 @@ import utilities.BaseTest;
 public class HomePage extends PageObject {
     private final String URL_HOME_PAGE = BaseTest.getBaseUrl();
     private final By SELECTOR_INPUT_SEARCH = By.id("search");
-    private final By SELECTOR_SEARCH_BUTTON = By.cssSelector("button[title='Search']");
 
     public void openUrl(){
         openUrl(URL_HOME_PAGE);
@@ -18,10 +17,7 @@ public class HomePage extends PageObject {
 
     public void searchKeyword(String keyword) {
         waitFor(ExpectedConditions.visibilityOfElementLocated(SELECTOR_INPUT_SEARCH));
-        typeInto(find(SELECTOR_INPUT_SEARCH), keyword);
-
-        waitFor(ExpectedConditions.elementToBeClickable(SELECTOR_SEARCH_BUTTON));
-        clickOn(find(SELECTOR_SEARCH_BUTTON));
+        typeInto(find(SELECTOR_INPUT_SEARCH), keyword + Keys.ENTER);
 
         waitFor(ExpectedConditions.urlContains("catalogsearch"));
     }
